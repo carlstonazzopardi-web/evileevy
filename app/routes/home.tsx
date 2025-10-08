ba0c48a4cecb3ba186efc3583649ce3802a0c450
carlstonazzopardi-web <carlstonazzopardi@gmail.com>
2025-10-08 10:50:39 +0200

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -14,7 +14,7 @@ export function loader({ context }: Route.LoaderArgs) {
 function HeaderBar(){
     return (
         <header class = "bg-black m-1">
-            <div class = "inline">
+            <div class = "flex">
             <p class = "text-white">EƎ</p>
                 <div class = "text-white">
                     <button>Home</button>
